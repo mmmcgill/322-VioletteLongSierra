f3a5704129bc1dfe5a3bc169ed47c8bbdc6100d1
Max Warren <mlwarren@knox.edu>
2020-05-27 19:38:39 -0400

    notification
    notification

---
+++ Hello There!/Assets/Script/NotificationManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Notifications.Android;
+
+public class NotificationManager : MonoBehaviour
+{
+    private void Start()
+    {
+        var c = new AndroidNotificationChannel()
+        {
+            Id = "channel_id",
+            Name = "Default Channel",
+            Importance = Importance.High,
+            Description = "Generic notifications",
+        };
+        AndroidNotificationCenter.RegisterNotificationChannel(c);
+
+        var notification = new AndroidNotification();
+        notification.Title = "New Reminder";
+        notification.Text = "You have a new reminder!";
+        notification.FireTime = System.DateTime.Now.AddSeconds(5);
+        notification.ShouldAutoCancel = true;
+
+        AndroidNotificationCenter.SendNotification(notification, "channel_id");
+    }
+}
+
